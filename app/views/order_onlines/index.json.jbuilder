aebfa3d3c0c32361438d2bc197a9eeb68a16a036
mahsa <m_mw7462@yahoo.com>
2014-06-03 12:33:07 +0430

    add user scaffold for admin, add home, hotel, office text as en an fa for BMS

--- app/views/order_onlines/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@order_onlines) do |order_online|
-  json.extract! order_online, :id, :name, :phone, :email, :description, :company_name, :web_site, :mobile, :text_msg
-  json.url order_online_url(order_online, format: :json)
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140521090805) do
+ActiveRecord::Schema.define(version: 20140603064048) do
 
   create_table "design_samples", force: true do |t|
     t.string   "name"
@@ -91,6 +91,14 @@ ActiveRecord::Schema.define(version: 20140521090805) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
