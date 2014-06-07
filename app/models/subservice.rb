e87ac8a9ee76f931960c40fc8aa6774aea08c531
mahsa <m_mw7462@yahoo.com>
2014-06-07 09:07:23 +0430

    add paperclip for subservice, and fix footer font-size

--- app/models/subservice.rb
+++ app/models/subservice.rb
@@ -1,3 +1,17 @@
+# encoding: UTF-8
 class Subservice < ActiveRecord::Base
   belongs_to :service
+  
+  has_attached_file :image, :styles => { :large => "500x500>" , :medium => "300x300>", :small => "200x200>" }
+  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"],
+                                            :message =>  "فرمت عکس صحیح نیست"
+                                            
+  def title
+    I18n.locale == :fa ? self.read_attribute("title_fa") : self.read_attribute("title_en")
+  end
+  
+  def text
+    I18n.locale == :fa ? self.read_attribute("text_fa") : self.read_attribute("text_en")
+  end                                          
+  
 end
--- app/views/subservices/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @subservice, :id, :text_fa, :text_en, :title_en, :title_fa, :created_at, :updated_at
