d143ef0cc4510a16ea04570cab7a4d2c1daab67c
mahsa <m_mw7462@yahoo.com>
2015-04-25 14:24:27 +0430

    check print factor

--- db/migrate/20150411133118_remove_paid_feild_from_account_document.rb
+++
@@ -1,6 +0,0 @@
-class RemovePaidFeildFromAccountDocument < ActiveRecord::Migration
-  def change
-    remove_column :account_documents, :paid_to, :integer
-    remove_column :account_documents, :paid_by, :integer
-  end
-end
--- db/migrate/20150425094948_add_paid_feilds_to_account_document.rb
+++ db/migrate/20150425094948_add_paid_feilds_to_account_document.rb
@@ -1,6 +1,6 @@
 class AddPaidFeildsToAccountDocument < ActiveRecord::Migration
   def change
-    add_column :account_documents, :paid_to, :string
     add_column :account_documents, :paid_by, :string
+    add_column :account_documents, :paid_to, :string
   end
 end
