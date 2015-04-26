d105cd70edfe683317b1bfb2b477ade18cf1b9c5
mahsa <m_mw7462@yahoo.com>
2015-04-26 09:15:31 +0430

    add datepicker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-
 //= require autocomplete-rails
+
 //= require ckeditor-jquery
 //= require dataTables/jquery.dataTables
 //= require jquery_nested_form
